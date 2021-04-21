b6dc7fd42e176c068148cbc65d632361ff576cda
dzastinka <jslawska5@gmail.com>
2021-04-21 15:49:55 +0200

    add unitOfMeasure class

--- src/main/java/guru/springframework/recipeapp/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/recipeapp/domain/UnitOfMeasure.java
@@ -6,26 +6,16 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import java.math.BigDecimal;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Setter
 @Entity
-public class Ingredient {
-
+public class UnitOfMeasure {
 @Id
 @GeneratedValue(strategy = IDENTITY)
 private Long id;
 private String description;
-private BigDecimal amount;
 
-@OneToOne
-private UnitOfMeasure unitOfMeasure;
-
-@ManyToOne
-private Recipe recipe;
 }
