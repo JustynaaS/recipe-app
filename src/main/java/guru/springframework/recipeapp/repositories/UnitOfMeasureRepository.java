24f5868dac1c2949261581e9d35d1513d79d3b8d
dzastinka <jslawska5@gmail.com>
2021-04-22 21:50:03 +0200

    add repositories

--- src/main/java/guru/springframework/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -1,6 +1,11 @@
 package guru.springframework.recipeapp.repositories;
 
+import guru.springframework.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasureRepository, Long> {
+import java.util.Optional;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+Optional<UnitOfMeasure> findByDescription(String description);
 }
